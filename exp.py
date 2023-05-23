013bece3af990abaa930e6017244f5015e0a46cc
Pyxlick <kamaevsergei1@gmail.com>
2023-05-23 22:28:47 +0300

    add 6 labs

---
+++ exp.py
@@ -0,0 +1,156 @@
+import pygame
+import sys
+from OpenGL.GL import *
+from OpenGL.GLU import *
+x=0
+y=0
+vx=0.001
+vy=-0.002
+vertices = (
+    # x  y  z
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+
+def loadTexture():
+    textureSurface = pygame.image.load('test2.png')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
+
+
+def draw_cube(lines=False):
+    global x
+    global vx
+    global vy
+    global y
+    if lines:
+        glBegin(GL_LINES)
+        for edge in edges:
+            glColor3fv((1, 1, 1))
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+    else:
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x-1.0, y-1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x+1.0, y-1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x+1.0,  y+1.0,  1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x-1.0,  y+1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x-1.0, y-1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x-1.0,  y+1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x+1.0, y+1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x+1.0, y-1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x-1.0,  y+1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x-1.0,  y+1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x+1.0,  y+1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x+1.0,  y+1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x-1.0, y-1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x+1.0, y-1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x+1.0, y-1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x-1.0, y-1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x+1.0, y-1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x+1.0,  y+1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x+1.0,  y+1.0,  1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x+1.0, y-1.0,  1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(x-1.0, y-1.0, -1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(x-1.0, y-1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(x-1.0,  y+1.0,  1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(x-1.0,  y+1.0, -1.0)
+        glEnd()
+    x+=vx
+    y+=vy
+    if x>=2 or x<=-2:
+        vx=-vx
+    if y>=2 or y<=-2:
+        vy=-vy
+pygame.init()
+display = (1920, 1080)
+display = (1920, 1080)
+screen = pygame.display.set_mode(
+    display, pygame.DOUBLEBUF | pygame.OPENGL | pygame.OPENGLBLIT)
+
+loadTexture()
+
+gluPerspective(45, display[0] / display[1], 0.1, 50.0)
+glTranslatef(0.0, 0.0, -5)
+
+# источники света
+
+#glLight(GL_LIGHT0, GL_POSITION, (5, 5, 5, 1))  # позиции света справа, сверху, со стороны наблюдателя
+#glLightfv(GL_LIGHT0, GL_AMBIENT, (0, 0, 0, 1))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
+
+glEnable(GL_DEPTH_TEST)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    draw_cube(lines=False)
+
+    pygame.display.flip()
--- lab6.py
+++ lab6.py
@@ -1,148 +1,138 @@
 import pygame
-import sys
+from pygame.locals import *
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
-x=0
-y=0
-vx=0.001
-vy=-0.002
-vertices = (
-    # x  y  z
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-)
+x,y = 0,0
+vx,vy = 0.01,0.06
+
+
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6),
+    )
+
+normals = [
+    ( 0,  0, -1),
+    (-1,  0,  0),
+    ( 0,  0,  1),
+    ( 1,  0,  0),
+    ( 0,  1,  0),
+    ( 0, -1,  0)
+]
+
+colors = (
+    (1,1,1),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (0,0,1),
+    (1,0,1),
+    (0,1,0),
+    (1,0,1),
+    (0,1,0),
+    (0,0,1),
+    )
 
 edges = (
-    (0, 1),
-    (0, 3),
-    (0, 4),
-    (2, 1),
-    (2, 3),
-    (2, 7),
-    (6, 3),
-    (6, 4),
-    (6, 7),
-    (5, 1),
-    (5, 4),
-    (5, 7)
-)
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7),
+    )
 
 
-def loadTexture():
-    textureSurface = pygame.image.load('browser_NjYzkBq2nq.png')
-    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
-    width = textureSurface.get_width()
-    height = textureSurface.get_height()
+def Cube():
+    global x,y
+    glBegin(GL_QUADS)
+    verticies = (
+        (x + 1, y - 1, -1),
+        (x + 1, y + 1, -1),
+        (x - 1, y + 1, -1),
+        (x - 1, y - 1, -1),
+        (x + 1, y - 1, 1),
+        (x + 1, y + 1, 1),
+        (x - 1, y - 1, 1),
+        (x - 1, y + 1, 1),
+    )
+    for i_surface, surface in enumerate(surfaces):
+        n = 0
+        glNormal3fv(normals[i_surface])
+        for vertex in surface:
+            n+=1
+            glColor3fv(colors[n])
+            glVertex3fv(verticies[vertex])
+    glEnd()
 
-    glEnable(GL_TEXTURE_2D)
-    texid = glGenTextures(1)
-
-    glBindTexture(GL_TEXTURE_2D, texid)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
-                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-
-    return texid
+    glColor3fv(colors[0])
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
 
 
-def draw_cube(lines=False):
-    global x
-    global vx
-    global vy
-    global y
-    if lines:
-        glBegin(GL_LINES)
-        for edge in edges:
-            glColor3fv((1, 1, 1))
-            for vertex in edge:
-                glVertex3fv(vertices[vertex])
-        glEnd()
-    else:
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x-1.0, y-1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x+1.0, y-1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x+1.0,  y+1.0,  1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x-1.0,  y+1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x-1.0, y-1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x-1.0,  y+1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x+1.0, y+1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x+1.0, y-1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x-1.0,  y+1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x-1.0,  y+1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x+1.0,  y+1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x+1.0,  y+1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x-1.0, y-1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x+1.0, y-1.0, -1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x+1.0, y-1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x-1.0, y-1.0,  1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x+1.0, y-1.0, -1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x+1.0,  y+1.0, -1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x+1.0,  y+1.0,  1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x+1.0, y-1.0,  1.0)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(x-1.0, y-1.0, -1.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(x-1.0, y-1.0,  1.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(x-1.0,  y+1.0,  1.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(x-1.0,  y+1.0, -1.0)
-        glEnd()
-    x+=vx
-    y+=vy
-    if x>=1 or x<=-1:
-        vx=-vx
-    if y>=1 or y<=-1:
-        vy=-vy
-pygame.init()
-display = (1920, 1080)
-display = (1920, 1080)
-screen = pygame.display.set_mode(
-    display, pygame.DOUBLEBUF | pygame.OPENGL | pygame.OPENGLBLIT)
+def main():
+    global surfaces
+    global x,y,vx,vy
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    clock = pygame.time.Clock()
 
-loadTexture()
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-gluPerspective(45, display[0] / display[1], 0.1, 50.0)
-glTranslatef(0.0, 0.0, -5)
+    glMatrixMode(GL_MODELVIEW)
+    glTranslatef(0, 0, -5)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    #источники света
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLight(GL_LIGHT0, GL_POSITION,  (5, 5, 5, 1)) # позиции света справа, сверху, со стороны наблюдателя
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0, 0, 0, 1))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
 
-    draw_cube(lines=False)
+    glEnable(GL_DEPTH_TEST)
 
-    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE )
+
+        glRotatef(1, 3, 1, 1)
+        Cube()
+        x+=vx
+        y+=vy
+        if x>=1 or x<=-1:
+            vx=-vx
+        if y>=1 or y<=-1:
+            vy=-vy
+        glDisable(GL_LIGHT0)
+        glDisable(GL_LIGHTING)
+        glDisable(GL_COLOR_MATERIAL)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+main()
